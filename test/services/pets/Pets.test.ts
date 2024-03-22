a1c0b394158cf7fe845d8828e7d8b919b823fcc3
david-liblab <144702126+david-liblab@users.noreply.github.com>
2024-03-21 20:05:38 -0400

    liblab SDK update for version v1.0.0

---
+++ test/services/pets/Pets.test.ts
@@ -0,0 +1,58 @@
+import nock from 'nock';
+
+import { Petstoresdk } from '../../../src';
+
+import { PetsService } from '../../../src/services/pets/Pets';
+
+describe('test PetsService object', () => {
+  it('should be an object', () => {
+    expect(typeof PetsService).toBe('function');
+  });
+});
+
+describe('test Pets', () => {
+  let sdk: any;
+
+  beforeEach(() => {
+    sdk = new Petstoresdk({});
+
+    nock.cleanAll();
+  });
+
+  describe('test listPets', () => {
+    test('test api call', () => {
+      const scope = nock('http://petstore.swagger.io/v1')
+        .get('/pets?limit=1')
+        .reply(200, { data: {} });
+      return sdk.pets.listPets({ limit: 1 }).then((r: any) => expect(r.data).toEqual({}));
+    });
+  });
+
+  describe('test createPets', () => {
+    test('test api call', () => {
+      const scope = nock('http://petstore.swagger.io/v1').post('/pets').reply(200, { data: {} });
+      return sdk.pets.createPets({}).then((r: any) => expect(r.data).toEqual({}));
+    });
+  });
+
+  describe('test showPetById', () => {
+    test('test api call', () => {
+      const scope = nock('http://petstore.swagger.io/v1')
+        .get('/pets/accusantium')
+        .reply(200, { data: {} });
+      return sdk.pets.showPetById('accusantium').then((r: any) => expect(r.data).toEqual({}));
+    });
+
+    test('test will throw error if required fields missing', () => {
+      const scope = nock('http://petstore.swagger.io/v1').get('/pets/eum').reply(200, { data: {} });
+      return expect(async () => await sdk.pets.showPetById()).rejects.toThrow();
+    });
+
+    test('test will throw error on a non-200 response', () => {
+      const scope = nock('http://petstore.swagger.io/v1')
+        .get('/pets/quibusdam')
+        .reply(404, { data: {} });
+      return expect(async () => await sdk.pets.showPetById('quibusdam')).rejects.toThrow();
+    });
+  });
+});
